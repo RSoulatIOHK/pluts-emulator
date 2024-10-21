da11b74b3a4d726bf3796e739828d52cf4b121af
Romain Soulat <117812549+RSoulatIOHK@users.noreply.github.com>
2024-10-21 18:14:24 +0200

    local experiments

--- src/Emulator.ts
+++ src/Emulator.ts
@@ -1,6 +1,35 @@
-import { Address, AddressStr, CanBeTxOutRef, defaultProtocolParameters, forceTxOutRefStr, Hash32, isProtocolParameters, IUTxO, ProtocolParameters, StakeAddressBech32, Tx, TxOutRefStr, UTxO } from "@harmoniclabs/cardano-ledger-ts"
+import { Address, AddressStr, CanBeTxOutRef, TxOutRef, defaultProtocolParameters, forceTxOutRefStr, Hash32, isProtocolParameters, IUTxO, ProtocolParameters, StakeAddressBech32, Tx, TxOutRefStr, UTxO } from "@harmoniclabs/cardano-ledger-ts"
 import { StakeAddressInfos } from "./types/StakeAddressInfos";
-import { CanBeData, CanResolveToUTxO, defaultMainnetGenesisInfos, GenesisInfos, IGetGenesisInfos, IGetProtocolParameters, IProvider, IResolveDatumHashes, IResolveUTxOs, isGenesisInfos, ISubmitTx, normalizedGenesisInfos, NormalizedGenesisInfos, TxBuilder, TxBuilderRunner } from "@harmoniclabs/plu-ts-offchain"
+import { CanBeData,  CanResolveToUTxO, defaultMainnetGenesisInfos, GenesisInfos, IGetGenesisInfos, IGetProtocolParameters, IProvider, IResolveDatumHashes, IResolveUTxOs, isGenesisInfos, ISubmitTx, normalizedGenesisInfos, NormalizedGenesisInfos, TxBuilder, TxBuilderRunner } from "@harmoniclabs/plu-ts-offchain"
+import { debug } from "console";
+
+class Queue<T> {
+    private items: T[] = [];
+
+    enqueue(item: T): void {
+        this.items.push(item);
+    }
+
+    dequeue(): T | undefined {
+        return this.items.shift();
+    }
+
+    peek(): T | undefined {
+        return this.items[0];
+    }
+
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+
+    asArray(): T[] {
+        return this.items;
+    }
+}
 
 export class Emulator
 implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
@@ -9,8 +38,8 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
     private readonly stakeAddresses: Map<StakeAddressBech32, StakeAddressInfos>;
     private readonly addresses: Map<AddressStr, Set<TxOutRefStr>>;
 
-    /** to be validated on block creation; the user might want to test utxo contention scenarios */
-    private readonly mempool: Tx[];
+    private debugLevel: number;
+    private readonly mempool: Queue<Tx>;
 
     private time: number;
     private slot: number;
@@ -23,7 +52,8 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
     constructor(
         initialUtxoSet: Iterable<IUTxO> = [],
         genesisInfos: GenesisInfos = defaultMainnetGenesisInfos,
-        protocolParameters: ProtocolParameters = defaultProtocolParameters
+        protocolParameters: ProtocolParameters = defaultProtocolParameters,
+        debugLevel: number = 1,
     )
     {
         if( !isGenesisInfos( genesisInfos ) ) genesisInfos = defaultMainnetGenesisInfos;
@@ -33,6 +63,8 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
         this.protocolParameters = protocolParameters;
 
         this.txBuilder = new TxBuilder( this.protocolParameters, this.genesisInfos );
+        this.mempool = new Queue<Tx>();
+        this.debugLevel = debugLevel;
 
         this.time = this.genesisInfos.systemStartPosixMs;
         this.slot = this.genesisInfos.startSlotNo;
@@ -46,7 +78,10 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
             this.pushUtxo( new UTxO( iutxo ) );
         }
     }
-
+    getUtxos(): Map<TxOutRefStr, UTxO>
+    {
+        return new Map( this.utxos );
+    }
     private pushUtxo( utxo: UTxO ): void
     {
         const ref = utxo.utxoRef.toString();
@@ -62,6 +97,37 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
         }
     }
 
+    // awaitBlock:
+    // go through the mempool
+    //    - For each transaction:
+    //       - If yes:
+    //          - consume the UTxOs in input (remove from utxo)
+    //          - add the UTxOs in output (add to utxo)
+    //       - If no:
+    //          - remove the transaction from the mempool
+    // Moves time forward to the next blockNumber block
+    awaitBlock (blockNum : number) : void {
+        if (blockNum > 0){
+            while (!this.mempool.isEmpty()){
+                const tx = this.mempool.dequeue()!;
+                for (let i = 0; i < tx.body.inputs.length; i++){
+                    this.removeUtxo(tx.body.inputs[i])
+                }
+                // utxoRef { id : TxId?, index : index in the TxOut}
+                for (let i = 0; i < tx.body.outputs.length; i++){
+                    this.pushUtxo(new UTxO({
+                        resolved: tx.body.outputs[i],
+                        utxoRef: new TxOutRef({
+                            id: new Hash32('a'.repeat(64)),
+                            index: i
+                        })
+                    }))
+                }
+            }
+        }
+        else {}
+    }
+   
     private removeUtxo( utxoRef: CanBeTxOutRef ): void
     {
         const ref = forceTxOutRefStr( utxoRef );
@@ -98,8 +164,76 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
         return Promise.resolve(
             [ ...new Set<TxOutRefStr>( utxos.map( forceTxOutRefStr ) ) ]
             .map( ref => this.utxos.get( ref )?.clone() )
-            .filter( u => u instanceof UTxO )
+            .filter( u => u instanceof UTxO ) as UTxO[]
         );
+    }
+
+    /**
+     * Print the mempool
+     */
+    printMempool(): void {
+        console.log("Mempool:");
+        for (let i = 0; i < this.mempool.size(); i++){
+            console.log("Transaction ID:", this.mempool.asArray()[i].hash.toString());
+            console.log("Transaction Inputs:");
+            for (let j = 0; j < this.mempool.asArray()[i].body.inputs.length; j++){
+                console.log("  Input ID:", this.mempool.asArray()[i].body.inputs[j].toString());
+            }
+            console.log("Transaction Outputs:");
+            for (let j = 0; j < this.mempool.asArray()[i].body.outputs.length; j++){
+                console.log("  Output ID:", this.mempool.asArray()[i].body.outputs[j].toString());
+            }
+        }
+    }
+    /**
+     * Print one UTXO
+     */
+    printUtxo(utxo: UTxO, debugLevel: number = 1): void {
+        console.log("UTxO Ref ID:", utxo.utxoRef.id.toString());
+        console.log("UTxO Ref Index:", utxo.utxoRef.index);
+        if (debugLevel > 2) {
+            console.log("Address Type:", utxo.resolved.address.type);
+        }
+        if (debugLevel > 1) {
+            console.log("Network:", utxo.resolved.address.network);
+        }
+        if (debugLevel > 1) {
+            console.log("Payment Credentials:", utxo.resolved.address.paymentCreds);
+        } else {
+            console.log("Payment Credentials (hex):", utxo.resolved.address.paymentCreds.hash.toString());
+        }
+        if (utxo.resolved.address.stakeCreds) {
+            console.log("Stake Credentials:", utxo.resolved.address.stakeCreds);
+        }
+        console.log("Value: {");
+        console.log("  Assets:");
+        utxo.resolved.value.map.forEach(asset => {
+            console.log("    Policy:", asset.policy);
+            asset.assets.forEach(a => {
+                if (a.name.toString() == "") {
+                    console.log("      Asset: lovelaces");
+                }
+                else {
+                    console.log("      Asset:", a.name);
+                }
+                console.log("      Quantity:", a.quantity);
+            });
+        });
+        if (utxo.resolved.datum) {
+            console.log("  Datum:", utxo.resolved.datum);
+        }
+        if (utxo.resolved.refScript) {
+            console.log("  Reference Script:", utxo.resolved.refScript);
+        }
+        console.log("}");
+    }
+    /**
+     * Print all the UTXOs
+     */
+    printUtxos(utxos: Map<TxOutRefStr, UTxO>, debugLevel: number = 1): void {
+        for (let utxo of utxos.values()){
+            this.printUtxo(utxo, debugLevel);
+        }
     }
 
     /**
@@ -109,9 +243,19 @@ implements IGetGenesisInfos, IGetProtocolParameters, IResolveUTxOs, ISubmitTx
     {
         // TODO add some tx validation
         const tx = txCBOR instanceof Tx ? txCBOR : Tx.fromCbor( txCBOR );
-
-        this.mempool.push( tx );
+        if (this.isTxValid(tx)) {
+            this.mempool.enqueue( tx );
+        }
+        else {
+            // Add a debug level and some more useful information
+            console.log("Transaction is invalid")
+        }
 
         return Promise.resolve( tx.hash.toString() );
+    }
+    
+    private isTxValid(tx: Tx) : boolean {
+        // Check if the transaction is valid
+        return true
     }
 }
